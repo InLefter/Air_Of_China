fcc9984639b5278f69110202e4c286fbc7c87594
Left_In <InLefter@users.noreply.github.com>
2017-03-14 12:19:23 +0800

    remove example mysql connection

--- CityRealTimeSpider.py
+++ CityRealTimeSpider.py
@@ -5,6 +5,3 @@ import Spider
 if __name__ == "__main__":
     obj = Spider.SpiderMain()
     obj.getCityRealTimeInfo()
-    obj.con_cursor.close()
-    obj.connection.commit()
-    obj.connection.close()
